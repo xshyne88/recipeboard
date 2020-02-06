5423ad33b2825d0297253a4f681db428320fb936
Chase Philips <chasehomedecor@gmail.com>
2020-02-05 19:00:12 -0500

    setup fail test

--- test/recipeboard_web/schema/user.ex
+++ test/recipeboard_web/schema/user.ex
@@ -1,18 +1,17 @@
 defmodule RecipeboardWeb.Schema.UserTest do
   alias Recipeboard.Accounts.User
+  use Wormwood.GQLCase
 
   use ExUnit.Case, async: true
 
-  test "users query" do
-    testQuery("user.graphql") |> IO.inspect()
-  end
+  load_gql RecipeboardWeb.Schema, "assets/user.graphql"
 
-  text "create users mutation creates a new user" do
-    input
-    {:ok, result} = testQuery("create_user.graphql") |> IO.inspect()
+  # test "users query" do
+  #   testQuery("user.graphql") |> IO.inspect()
+  # end
 
-    assert(expected == %User{
-      email: "foo"
-    })
+  test "create users mutation creates a new user" do
+    input = %{}
+    query_gql(variables: %{}, context: %{}) |> IO.inspect()
   end
 end
