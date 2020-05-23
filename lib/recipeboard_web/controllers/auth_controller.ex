c975dffb8a11ff68533cced0fe7a201b64b251b9
Chase Philips <chasephilips@chases-mbp.home>
2020-05-23 11:55:25 -0400

    WIP Log oauth2 result

--- lib/recipeboard_web/controllers/auth_controller.ex
+++ lib/recipeboard_web/controllers/auth_controller.ex
@@ -1,14 +1,22 @@
 defmodule RecipeboardWeb.AuthController do
   use RecipeboardWeb, :controller
+  alias Ueberauth
+
   plug Ueberauth
 
   def callback(conn, params) do
     conn
     |> IO.inspect(label: "conn")
+    |> scrub_oauth2()
 
     params
-    |> IO.inspect(label: "params")
+    # |> IO.inspect(label: "params")
 
     conn |> redirect(external: "/graphiql")
+  end
+
+  defp scrub_oauth2(%{assigns: %Auth{}, provider: provider, uid: uid}) do
+    IO.inspect(provider, label: "provider: ")
+    IO.inspect(uid, label: "provider: ")
   end
 end
