86f4354095bffbb23963dc9ad21916f5ec0e36ee
Chase Philips <chasephilips@chases-mbp.home>
2020-05-23 01:04:12 -0400

    add oauth2 stuff to router

--- lib/recipeboard_web/router.ex
+++ lib/recipeboard_web/router.ex
@@ -6,10 +6,23 @@ defmodule RecipeboardWeb.Router do
     plug :accepts, ["json"]
   end
 
+  # pipeline :browser do
+  # plug RecipeboardWeb.Auth
+  # end
+
   scope "/api" do
     pipe_through [:api]
 
     forward "/", Absinthe.Plug, schema: RecipeboardWeb.Schema
+  end
+
+  scope "/auth", RecipeboardWeb do
+    # pipe_through :browser
+
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+    post "/:provider/callback", AuthController, :callback
+    post "/logout", AuthController, :delete
   end
 
   scope "/graphiql" do
