b5f7f2a436b9cc6716390172a149d19735d3bf25
Chase Philips <chasephilips@chases-mbp.home>
2020-05-25 22:43:34 -0400

    add oauth2 functionality to accounts

--- lib/recipeboard/accounts.ex
+++ lib/recipeboard/accounts.ex
@@ -1,8 +1,21 @@
 defmodule Recipeboard.Accounts do
   import Ecto.Query, warn: false
+  alias Ecto.Multi
   alias Recipeboard.Repo
-
   alias Recipeboard.Accounts.User
+  alias RecipeboardWeb.Guardian
+
+  def register(%{provider: :facebook} = params) do
+    with {:ok, %{upserted_user: %{email: email, image: image}}} <- upsert_user(params, tx: true),
+         {:ok, token, _claims} <- Guardian.encode_and_sign(%{email: email, image: image}) do
+      {:ok, token}
+    else
+      {:error, _} = tuple_error -> tuple_error
+      err -> {:error, err}
+    end
+  end
+
+  def register(_), do: {:error, "check register oauth function, something went wrong"}
 
   def query_data() do
     Dataloader.Ecto.new(Repo, query: &query/2)
@@ -21,6 +34,18 @@ defmodule Recipeboard.Accounts do
   end
 
   def get_user!(id), do: Repo.get!(User, id)
+
+  def upsert_user(params, tx: true) do
+    Multi.new()
+    |> Multi.run(:upserted_user, fn _, _ -> upsert_user(params) end)
+    |> Repo.transaction()
+  end
+
+  def upsert_user(%{email: email} = params) do
+    %User{}
+    |> User.changeset(params)
+    |> Repo.insert(on_conflict: :nothing)
+  end
 
   def create_user(attrs \\ %{}) do
     %User{}
